077251f058a0a354b6c0cfbb1d3946b4e668c4ad
Mongkhon650 <65022027@up.ac.th>
2024-02-03 17:17:44 +0700

    Update ALL Website

---
+++ JSFILE/Homepage.js
@@ -0,0 +1,8 @@
+function showFilterBox() {
+    var filterBox = document.getElementById('filterBox');
+    if (filterBox.style.display === 'none') {
+        filterBox.style.display = 'block';
+    } else {
+        filterBox.style.display = 'none';
+    }
+}
